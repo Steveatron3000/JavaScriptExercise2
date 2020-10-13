059b90d83806e089d840e2eed185e6a80fff4a9f
Steveatron3000 <j.r.franklin@aol.com>
2020-10-13 13:04:55 -0500

    Ready

---
+++ Exercise2.js
@@ -0,0 +1,15 @@
+let myAge =17;
+//Age minus 5
+console.log(myAge-5)
+//age plus six
+console.log(myAge+6);
+//age minus 1
+console.log(myAge-1)
+
+//Age none of your business.
+console.log(myAge = "None "+"Of "+"Your "+"Business")
+
+//Blank
+let blank="";
+console.log(blank=null);
+
